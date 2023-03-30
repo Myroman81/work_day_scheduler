bebe944b13448ee449df2dcd029ac1f48afd7806
myroman-wuLtCL <myroman@protonmail.com>
2023-03-30 18:16:35 -0400

    update post tutor session

--- assets/script.js
+++ assets/script.js
@@ -12,32 +12,43 @@
     //$("#date").val( moment().format('MMM D, YYYY') );
 
     // set a variable
-    //var today = moment().format('D MMM, YYYY');
+    //var today = moment().format('MMM DD, YYYY');
 
 //});
 var today = new Date();
-document.getElementById('currentDay').value += today;
+var now = dayjs();
+console.log("Date: ", today);
+console.log("Day Date: ", now.format('dddd MMM DD h:mm'));
+document.getElementById('currentDay').textContent = now.format('dddd MMM h:mm a');
 
 
 $(document).ready(function () {
     // saveBtn click listener 
+    // document.querySelectorAll('.saveBtn')
     $(".saveBtn").on("click", function () {
+        console.log("This: ", $(this))
         // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+        var text = $(this).siblings(".description").val();   // --> User input "bingo"
+        var time = $(this).parent().attr("id");  // --> "hour-8"
 
         // Save text in local storage
+        //                   KEY   VALUE
         localStorage.setItem(time, text);
+
+        // .getItem(*key*);
+        // .setItem(*key*, VALUE);
+        // .clear()  .removeItem()
     })
    
     function timeTracker() {
         //get current number of hours.
-        var timeNow = moment().hour();
+        var timeNow = dayjs().hour();
+        //console.log("Current Hour: ", timeNow);
 
         // loop over time blocks
         $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
+            var blockTime = parseInt($(this).attr("id").split("-")[1]);  // --> ["hour", 8]  
+            //console.log("Block: ", blockTime);
             
             if (blockTime < timeNow) {
                 $(this).removeClass("future");
@@ -57,6 +68,10 @@ $(document).ready(function () {
             }
         })
     }
+
+    var tenAM = localStorage.getItem('hour-10');
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    console.log("Saved Data: ", tenAM);
 
     
 
