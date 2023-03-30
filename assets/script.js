75a9083a6cf819469cd8df46dcca79b3f8c45b77
myroman-wuLtCL <myroman@protonmail.com>
2023-03-29 21:14:38 -0400

    update with styling

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,76 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+// Display today's day and date
+
+//var todayDate = lead().format('dddd, MMM, YYYY');
+//$("#currentDay").html(todayDate);
+
+//$(document).ready(function() {
+
+    // set an element
+    //$("#date").val( moment().format('MMM D, YYYY') );
+
+    // set a variable
+    //var today = moment().format('D MMM, YYYY');
+
+//});
+var today = new Date();
+document.getElementById('currentDay').value += today;
+
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+   
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    
+
+    timeTracker();
+})
+
+
+
+
+
+
+
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +89,3 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
